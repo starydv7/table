f33f4af76c5cac7c2f356cf3d06b963efb859ab1
starydv7 <pawanstar72926@gmail.com>
2023-01-04 17:03:02 +0530

    table

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Home  from "./components/Home";
 import './App.css';
 import Fetch from "./components/Fetch";
+import Bootstrap from "./components/App";
 function App() {
   return (
     <div className="App">
       <Home />
-      <Fetch/>
+      <Fetch />
+      <Bootstrap/>
     </div>
   );
 }
---
+++ src/components/App.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { MDBTable, MDBTableHead, MDBTableBody } from "mdb-react-ui-kit";
+
+export default function Bootstrap() {
+  return (
+    <MDBTable>
+      <MDBTableHead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">First</th>
+          <th scope="col">Last</th>
+          <th scope="col">Handle</th>
+        </tr>
+      </MDBTableHead>
+      <MDBTableBody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td colSpan={2}>Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </MDBTableBody>
+    </MDBTable>
+  );
+}
