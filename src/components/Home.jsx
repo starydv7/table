1ccb8240efaa0193265ca98d5f676be9c46f7163
starydv7 <pawanstar72926@gmail.com>
2023-01-04 13:10:38 +0530

    Table

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
-
- class Home extends Component {
-  render() {
+import ReactTable from "react-table";
+class Home extends Component {
+  state = {
+    books: [],
+  };
+  componentDidMount() {
+    fetch("https://dummyjson.com/users")
+      .then((response) => response.json())
+      .then((booksList) => {
+        this.setState({ books: booksList });
+        //console.log(booksList);
+      });
+  }
+    render() {
+        console.log("Books",this.state.books.users);
     return (
-      <div>Home</div>
-    )
+      <ul>
+        {this.state.books.map((book) => (
+          <li key={book.id}>{book.firstName}</li>
+        ))}
+      </ul>
+    );
   }
 }
 export default Home;
